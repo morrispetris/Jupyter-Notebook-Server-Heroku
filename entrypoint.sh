e422b5a4b3785a6cea8327587802fb2befb2a1cb
morrispetris <112695708+morrispetris@users.noreply.github.com>
2023-03-13 12:01:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+PASSWORD="$(< /etc/secrets/PASSWORD)"
+
 #jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.token='' --NotebookApp.password='' --allow-root
 jupyter notebook --ip=0.0.0.0 --port=$PORT --NotebookApp.password=$PASSWORD --allow-root
